e82667f3abc59cb903c0d1f7272fb559948cf31f
Adshya Iyer <adshy91@gmail.com>
2021-04-26 22:32:19 +0200

    Added a method to show the preprcossing pipeline

--- crack_preprocessing.py
+++ crack_preprocessing.py
@@ -12,6 +12,12 @@ class CrackDetector:
     def __init__(self):
         self._image = None
         self._gray_image = None
+        self._gaussian_image = None
+        self._thresholded_image = None
+        self._median_image = None
+        self._pooled_image = None
+        self._opened_image = None
+        self._closed_image = None
         self._mask = None
         self._blobs = None
         self._numberOfWhitePixels = 0
@@ -58,6 +64,36 @@ class CrackDetector:
 
     def ShowImage(self):
         plt.imshow(self._image)
+
+    def ShowImageGray(self):
+        plt.imshow(self._gray_image, cmap="gray")
+    
+    def ShowProcessingSteps(self, save = False):
+        fig, axs = plt.subplots(3, 3)
+        axs[0, 0].imshow(self._image)
+        axs[0, 0].set_title("Original")
+        axs[0, 1].imshow(self._gray_image, cmap="gray")
+        axs[0, 1].set_title("Grayscalling")
+        axs[0, 2].imshow(self._gaussian_image, cmap="gray")
+        axs[0, 2].set_title("Gaussian filtering")
+        axs[1, 0].imshow(self._thresholded_image, cmap="gray")
+        axs[1, 0].set_title("Thresholding")
+        axs[1, 1].imshow(self._median_image, cmap="gray")
+        axs[1, 1].set_title("Median filtering")
+        axs[1, 2].imshow(self._pooled_image, cmap="gray")
+        axs[1, 2].set_title("Max pooling")
+        axs[2, 0].imshow(self._opened_image, cmap="gray")
+        axs[2, 0].set_title("Opening")
+        axs[2, 1].imshow(self._closed_image, cmap="gray")
+        axs[2, 1].set_title("Closing")
+        axs[2, 2].imshow(self._mask, cmap="gray")
+        axs[2, 2].set_title("Mask")
+
+        for ax in axs.flat:
+            ax.axis('off')
+
+        if save:
+            fig.savefig('fig.jpg', dpi=150)
     
     def ShowMask(self):
         plt.imshow(self._mask, cmap="gray")
@@ -102,33 +138,32 @@ class CrackDetector:
     def __segmentImage(self):
         # gray scalling, gaussian filter, threshold, median blurring, max pooling then morphology
         self._gray_image = cv2.cvtColor(self._image, cv2.COLOR_BGR2GRAY)
-        self._mean = int(np.mean(self._gray_image))
+        self._mean = np.mean(self._gray_image)
         self._std = np.std(self._gray_image)
-        image = cv2.GaussianBlur(self._gray_image, (41, 41), 20)
+        self._gaussian_image = cv2.GaussianBlur(self._gray_image, (41, 41), 0)
 
         lower_range = np.array([0])
-        upper_range = np.array([int(self._mean-0.50*self._std)])
-        ranged_image = cv2.inRange(image, lower_range, upper_range)
-        blurred_image = cv2.medianBlur(ranged_image, 21)
-        pooled_image = block_reduce(blurred_image, (10, 10), np.max)
+        upper_range = np.array([int(self._mean-0.5*self._std)])
+        self._thresholded_image = cv2.inRange(self._gaussian_image, lower_range, upper_range)
+        self._median_image = cv2.medianBlur(self._thresholded_image, 21)
+        pooled_image = block_reduce(self._median_image, (10, 10), np.max)
 
-        pooled_image = np.asarray(pooled_image, np.uint8)
+        self._pooled_image = np.asarray(pooled_image, np.uint8)
 
-        opening_krnl = np.ones((3,3))
-        closing_krnl = np.ones((2,2))
-        opened_image = cv2.morphologyEx(pooled_image, cv2.MORPH_OPEN, opening_krnl)
-        closed_image = cv2.morphologyEx(opened_image, cv2.MORPH_CLOSE, closing_krnl)
+        kernel = np.ones((3,3))
+        self._opened_image = cv2.morphologyEx(self._pooled_image, cv2.MORPH_OPEN, kernel)
+        self._closed_image = cv2.morphologyEx(self._opened_image, cv2.MORPH_CLOSE, kernel)
         
-        contours, _ = cv2.findContours(closed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(self._closed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        blob_mask = np.zeros(closed_image.shape, dtype=np.uint8)
-        blobs_mask = np.zeros(closed_image.shape, dtype=np.uint8)
+        blob_mask = np.zeros(self._closed_image.shape, dtype=np.uint8)
+        blobs_mask = np.zeros(self._closed_image.shape, dtype=np.uint8)
 
         for i, contour in enumerate(contours):
             c_area = cv2.contourArea(contour)
             if c_area >= 100:
                 cv2.drawContours(blob_mask, contours, i, (255, 255, 255), cv2.FILLED)
-                blob_mask = cv2.bitwise_and(closed_image, blob_mask)
+                blob_mask = cv2.bitwise_and(self._closed_image, blob_mask)
                 blobs_mask = cv2.bitwise_or(blobs_mask, blob_mask)
         
         self._blobs = contours
