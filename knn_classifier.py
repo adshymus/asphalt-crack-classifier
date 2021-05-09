8eb2e328cf0cb9fb6e239e5d03d62d9db8f7995c
Adshya Iyer <adshy91@gmail.com>
2021-05-09 22:40:11 +0200

    Removed unused import

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,6 +1,5 @@
 from sklearn.neighbors import KNeighborsClassifier
 from crack_classifier import CrackClassifier
-import matplotlib.image as mpimg
 
 class KNNClassifier(CrackClassifier):
     def __init__(self, k):
